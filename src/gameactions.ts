1c06b3c585951565244c4f6bb9276d1df8a2181c
Paul Nicholas <liquidream@hotmail.co.uk>
2022-05-13 07:48:54 +0100

    WIP deaths

--- src/gameactions.ts
+++ src/gameactions.ts
@@ -6,18 +6,26 @@ export class Actions {
     //console.log("TODO: onEnterCaveTunnel()");
     if (SAGE.World.player.inInventory("prp_rat")) {
       console.log("TODO: Safe! Snake ate the rat...");
+      SAGE.Dialog.showMessage('A snake strikes and eats the rat & leaves');
     } else {
       console.log("TODO: DEAD!...");
+      /* 
+      A small snake is sleeping in its favourite cranny.
+      The serpent awakes, and vents its anger on you by biting you.
+      The venom is swift and painless in its fatal effect.
+      */
+      SAGE.Dialog.showErrorMessage('A hungry snake strikes and bites you - YOU DIED');
     }    
   }
 
-  onTreeAction(): void {
+  onTreeAction(): void {    
     let treeProp = SAGE.World.getPropById('prp_tree');
+    // If not collected key already...
     if (treeProp && !treeProp.property["key-collected"]) {
       SAGE.World.putPropAt('prp_key','scn_promontory');
       treeProp.property["key-collected"] = true;
       SAGE.Dialog.showMessage('A Key fell out of the tree');
-      console.log(treeProp.property["key-collected"]);
+      //console.log(treeProp.property["key-collected"]);
     }
   }
 }
